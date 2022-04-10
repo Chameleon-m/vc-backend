fa19cce480fc76ed52a49584121d0a337361d751
Dmitry Korolev <chameleonweb2012@gmail.com>
2022-04-11 00:04:08 +0300

    docker nginx beta

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
