465ac91df8c16ac8d37df3970670d8672e063a3d
Dmitry Korolev <chameleonweb2012@gmail.com>
2022-04-29 23:11:35 +0300

    UserDataProvider

---
+++ src/DataProvider/UserDataProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\DataProvider;
+
+use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\DenormalizedIdentifiersAwareItemDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
+
+class UserDataProvider implements
+    ContextAwareCollectionDataProviderInterface,
+    DenormalizedIdentifiersAwareItemDataProviderInterface,
+    RestrictedDataProviderInterface
+{
+    private CollectionDataProviderInterface $collectionDataProvider;
+    private Security $security;
+    private ItemDataProviderInterface $itemDataProvider;
+
+    public function __construct(CollectionDataProviderInterface $collectionDataProvider, ItemDataProviderInterface $itemDataProvider, Security $security)
+    {
+        $this->collectionDataProvider = $collectionDataProvider;
+        $this->security = $security;
+        $this->itemDataProvider = $itemDataProvider;
+    }
+
+    public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
+    {
+        /** @var User[] $users */
+        $users = $this->collectionDataProvider->getCollection($resourceClass, $operationName, $context);
+        // https://symfonycasts.com/screencast/api-platform-extending/item-data-provider
+//        $currentUser = $this->security->getUser();
+//        foreach ($users as $user) {
+//            $user->setIsMe($currentUser === $user);
+//        }
+        return $users;
+    }
+
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        return $resourceClass === User::class;
+    }
+
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ?User
+    {
+        /** @var User|null $item */
+        $item = $this->itemDataProvider->getItem($resourceClass, $id, $operationName, $context);
+        if (!$item) {
+            return null;
+        }
+//        $item->setIsMe($this->security->getUser() === $item);
+        return $item;
+    }
+}
